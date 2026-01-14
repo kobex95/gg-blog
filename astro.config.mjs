82e26c6a35307c6cdbd962cac36f90d3f7f9881d
kobex95 <kobex95@example.com>
2026-01-15 02:18:21 +0800

    Fix: Use static output with prerender disabled for admin dynamic routes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'hybrid',
+  output: 'static',
   integrations: [
     tailwind(),
     react()
