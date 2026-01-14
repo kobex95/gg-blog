1b66a6872aaf1e10fbd98489feaab5d057ef65ea
kobex95 <kobex95@example.com>
2026-01-15 02:08:06 +0800

    Fix: Remove invalid astro-node-functions import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,6 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import react from '@astrojs/react';
-import { nodeFunctions } from 'astro-node-functions';
 
 // https://astro.build/config
 export default defineConfig({
